9d7d380040371bc16155411cf89dd5abc1917c28
Yash Sikhwal <Yash.Sikhwal@in.bosch.com>
2024-09-04 14:12:13 +0530

    Made code changes to reduce CCN and added new test cases

--- Soundex.Tests.cpp
+++ Soundex.Tests.cpp
@@ -2,11 +2,26 @@
 #include "Soundex.h"
 
 
-TEST(SoundexTest, HandlesEmptyString) {
+TEST(SoundexTest, HandlesEmptyString) 
+{
     EXPECT_EQ(generateSoundex(""), "");
 }
 
-TEST(SoundexTest, HandlesSingleCharacter) {
+TEST(SoundexTest, HandlesSingleCharacter) 
+{
     EXPECT_EQ(generateSoundex("A"), "A000");
+    EXPECT_EQ(generateSoundex("Z"), "Z000");
+}
+
+TEST(SoundexTest, HandlesMultipleCharacters)
+{
+    EXPECT_EQ(generateSoundex("Smith"), "S530");
+    EXPECT_EQ(generateSoundex("Fletcher "), "F432");
+    EXPECT_EQ(generateSoundex("Smyth"), "S530");
+    EXPECT_EQ(generateSoundex("Jones"), "J520");
+    EXPECT_EQ(generateSoundex("Barnes"), "B652");
+    EXPECT_EQ(generateSoundex("Huey"), "H000");
+    EXPECT_EQ(generateSoundex("Zoe"), "Z000");
+    EXPECT_EQ(generateSoundex("Fay"), "F000");
 }
 
